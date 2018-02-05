01c7b38542db1e5ca5b8f38b6dddebee2e5def45
Dario Campagna <campagna@esteco.com>
2018-02-05 14:20:38 +0100

    Removed obsolete code

--- production/source/it/esteco/roman/numerals/RomanNumeral.java
+++ production/source/it/esteco/roman/numerals/RomanNumeral.java
@@ -10,12 +10,8 @@ public class RomanNumeral {
     public String toString() {
         if (decimal == 0) {
             return "";
-        } else if (decimal == 1) {
-            return "I";
-        } else if (decimal == 2) {
-            return "II";
         } else {
-            return "III";
+            return "I" + new RomanNumeral(decimal - 1).toString();
         }
     }
 }
--- to-do-list.md
+++ to-do-list.md
@@ -1,7 +1,7 @@
 ~~0 → Empty string~~  
-1 → I  
-2 → II  
-3 → III  
+~~1 → I~~  
+~~2 → II~~  
+~~3 → III~~  
 4 → IV  
 5 → V  
 6 → VI    
